8713489580f3bf9f0f8dc83677b505b517627370
Sean ODell <sean.odell@thingtech.com>
2016-09-06 16:17:48 -0400

    Weather Setup

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -1,14 +1,24 @@
 var React = require('react');
 
 var WeatherForm = React.createClass({
+  onFormSubmit: function (e) {  //the e (for event) argument will be passed into onFormSubmit
+    e.preventDefault();    //prevents the page from reloading
+
+    var location = this.refs.location.value;   //<--- so if the input field has the text Philadelphia inside, the location variable is set to Philly
+
+    if (location.length > 0) {
+      this.refs.location.value = '';  //this will clear the input value from the browser on submit
+      this.props.onSearch(location);  //calls parent function
+    }
+  },
 render: function () {
     return (
-      <form>
-        <div>
-          <input type="text" placeholder="Enter Location"/>
+      <div>
+        <form onSubmit={this.onFormSubmit}>
+          <input type="text" ref="location" placeholder="Enter Location"/>
           <button>Get Weather</button>
-        </div>
-      </form>
+        </form>>
+      </div>
     );
   }
 });
